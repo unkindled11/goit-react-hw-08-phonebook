9762dc3dcedf3dd9a2fa1ef0b7ec1b59517c29b5
Dmytro Kulakov <gambler11kulakov@gmail.com>
2022-06-13 07:54:30 +0300

    v3

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,7 @@
 import Section from 'shared/shared-components/section';
 
-import styles from './homePage.module.css';
-
 const HomePage = () => {
-  return (
-    <Section title="Go ahead! You can manage your contacts">
-      <iframe
-        className={styles.gif}
-        src="https://giphy.com/embed/26FfbEpKus6uPskM0"
-        width="580"
-        height="460"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-      <p>
-        <a href="https://giphy.com/gifs/heyarnold-hey-arnold-nicksplat-26FfbEpKus6uPskM0"></a>
-      </p>
-    </Section>
-  );
+  return <Section title="Go ahead! You can manage your contacts"></Section>;
 };
 
 export default HomePage;
